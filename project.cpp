a3f597f0c31c1ae844c4c8329f6413fc6cf7d0ce
kdigiorn <34196195+kdigiorn@users.noreply.github.com>
2017-12-06 20:57:44 -0500

    Add files via upload

--- project.cpp
+++ project.cpp
@@ -1,4 +1,3 @@
-// main driver
 #include "gfx.h"
 #include "enemyship.h"
 #include <stdio.h>
@@ -7,22 +6,54 @@
 #include <unistd.h>
 #include <iostream>
 
+void createShip(int, int, int);
+
 using namespace std;
 int main() {
   srand(time(NULL));
   gfx_open(510, 680, "Galaga");
-  Enemy en;
-  
-  
+  //Enemy en;
+  int x = 255, y = 664, rad = 15, y1 = 664;
+
+
   char c= 0;
   while (c != 'q') {
+    c = 0;
+    createShip(x, y, rad);
     if (gfx_event_waiting()) {
       c = gfx_wait();
       if (c == 'q')
-	return 0;
+        return 0;
+      else if(c == 's'){
+        gfx_line(x, y1 - rad, x, y1 - rad - 5);
+        gfx_flush();
+      }
+      else if(c == 'S'){
+        x += 5;
+      }
+      else if(c == 'Q'){
+        x -= 5;
+      }
     }
-    en.play();
-    usleep(2000000);
+    cout << c << endl;
+    gfx_clear();
+    //en.play();
+    /*if(c == 's'){
+      for(int i = 0; i < 28; i++){
+        gfx_clear();
+        createShip(x, y, rad);
+        gfx_line(x, y1 - rad, x, y1 - rad - 5);
+        gfx_flush();
+        usleep(715);
+      }
+    }*/
   }
   return 0;
 }
+
+void createShip(int x, int y, int rad){
+  gfx_line(x - rad, y, x + rad, y);
+  gfx_line(x + rad, y, x, y - rad);
+  gfx_line(x, y - rad, x - rad, y);
+}
+
