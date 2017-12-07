3e2fc5354ff739224e6e58a51a353caf1eff89df
kdigiorn <34196195+kdigiorn@users.noreply.github.com>
2017-12-06 21:59:09 -0500

    Add files via upload

--- project.cpp
+++ project.cpp
@@ -5,48 +5,54 @@
 #include <time.h>
 #include <unistd.h>
 #include <iostream>
-
-void createShip(int, int, int);
-
+#include <vector>
 using namespace std;
+void createShip(int, int, int);
+void moveShot();
+vector <int> xshots;
+vector <int> yshots;
+
 int main() {
   srand(time(NULL));
   gfx_open(510, 680, "Galaga");
   //Enemy en;
-  int x = 255, y = 664, rad = 15, y1 = 664;
-
+  int x = 255, y = 664, rad0 = 5, rad1 = 15, y1 = 664;
 
   char c= 0;
   while (c != 'q') {
     c = 0;
-    createShip(x, y, rad);
+    gfx_flush();
+    usleep(5000);
+    createShip(x, y, rad1);
     if (gfx_event_waiting()) {
       c = gfx_wait();
       if (c == 'q')
         return 0;
       else if(c == 's'){
-        gfx_line(x, y1 - rad, x, y1 - rad - 5);
-        gfx_flush();
+        gfx_circle(x, y1 - rad1 - rad0, rad0);
+        xshots.push_back(x);
+        yshots.push_back(y1 - rad1 - rad0);
       }
       else if(c == 'S'){
-        x += 5;
+        if(x > 495){
+          x = 495;
+        }
+        else{
+          x += 5;
+        }
       }
       else if(c == 'Q'){
-        x -= 5;
+        if(x < 15){
+          x = 15;
+        }
+        else{
+          x -= 5;
+        }
       }
     }
-    cout << c << endl;
     gfx_clear();
+    moveShot();
     //en.play();
-    /*if(c == 's'){
-      for(int i = 0; i < 28; i++){
-        gfx_clear();
-        createShip(x, y, rad);
-        gfx_line(x, y1 - rad, x, y1 - rad - 5);
-        gfx_flush();
-        usleep(715);
-      }
-    }*/
   }
   return 0;
 }
@@ -55,5 +61,14 @@ void createShip(int x, int y, int rad){
   gfx_line(x - rad, y, x + rad, y);
   gfx_line(x + rad, y, x, y - rad);
   gfx_line(x, y - rad, x - rad, y);
+}
+
+void moveShot(){
+  if(xshots.size() > 0){
+    for(int i = 0; i < xshots.size(); i++){
+      yshots[i] -= 5;
+      gfx_circle(xshots[i], yshots[i], 5);
+    }
+  }
 }
 
